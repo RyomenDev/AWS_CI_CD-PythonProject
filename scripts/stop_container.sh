6f12d27bcd0150e88b546bf8c8a74facc0321d2e
RyomenDev <aakashvatsh007@gmail.com>
2025-02-06 23:47:16 +0530

    fix:stop_container

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,4 +2,8 @@
 set -e
 
 # Stop the running container (if any)
-echo "Hi"
+# echo "Hi"
+# docker ps | aws -F " " '{print $1}' | tail -n +2 | xargs docker stop
+
+container_id=`docker ps | awk 'FNR == 2 {print $1}'`
+docker rm -f $container_id
